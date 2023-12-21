3c133325d8c38d10f0a17af7cdd1108605812aa4
CasaresEzequiel <casares.ezequiel@outlook.com>
2023-12-21 19:01:02 -0300

    Finalizada v1.0

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,6 @@ const Nav = () => {
           <img src={mikeLogo} alt="Logo" width={130} height={29} />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          {/*{`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}*/}
           {navLinks.map((link) => (
             <li key={link.label}>
               <a
@@ -24,14 +23,14 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
+        {/*<div className="hidden max-lg:block">
           <button
             onClick={() => setIsOpen(!isOpen)}
             className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
           >
             <img src={hamburger} alt="Hamburger" width={25} height={25}></img>
           </button>
-        </div>
+        </div>*/}
       </nav>
     </header>
   );
